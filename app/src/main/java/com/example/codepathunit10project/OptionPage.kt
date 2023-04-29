2ed4c5330b78859f5bc2eb1cc0e3435fc90ead93
Roy <lcgninja@gmail.com>
2023-04-29 05:08:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/codepathunit10project/OptionPage.kt
@@ -0,0 +1,11 @@
+package com.example.codepathunit10project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OptionPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option_page)
+    }
+}
